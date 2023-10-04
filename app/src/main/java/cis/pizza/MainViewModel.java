66a88ad418617fc79b286ceef09cb1b14e905b3d
tgibbons-css <tgibbons@css.edu>
2023-10-04 13:39:11 -0500

    Starting code for Unit 5 Learning Activity

---
+++ app/src/main/java/cis/pizza/MainViewModel.java
@@ -0,0 +1,95 @@
+package cis.pizza;
+
+
+import android.app.Application;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+//public class MainViewModel extends ViewModel {
+public class MainViewModel extends AndroidViewModel {
+
+    PizzaRepository pizzaOrder;
+    //List<String> pizzasOrder;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        pizzaOrder = new PizzaRepository(application);
+    }
+
+    // For live data updates from https://developer.android.com/topic/libraries/architecture/livedata
+    private MutableLiveData<String> orderStatus;
+    public MutableLiveData<String> getOrderStatus() {
+        if (orderStatus == null) {
+            orderStatus = new MutableLiveData<String>();
+        }
+        return orderStatus;
+    }
+
+    public void addToOrder(String topping, int iSize) {
+        Pizza newPizza = new Pizza(topping, iSize);
+        pizzaOrder.OrderPizza(newPizza);
+    }
+
+    public void placeOrder() {
+        orderStatus.postValue("Order Placed");
+        startPizzaTimer();
+    }
+
+    /*
+    public String getOrder() {
+        String orderText = "Pizzas: \n";
+        List<String> pizzasOrder = pizzaOrder.getOrder();
+        for (String strPizza:pizzasOrder ) {
+            orderText = orderText + "\n" + strPizza;
+        }
+        return orderText;
+    }
+    */
+
+    public String getOrderItem(Integer position) {
+        return pizzaOrder.getOrderItem(position);
+    }
+
+    public Integer getOrderSize() {
+        return pizzaOrder.getOrderSize();
+    }
+
+    /**
+     * This class implements a timer for the baking pizza
+     */
+    private static Runnable pizzaTimer;
+    private Handler handler;
+    private void startPizzaTimer(){
+        handler = new Handler();
+        pizzaTimer = new PizzaTimer();
+        handler.postDelayed(pizzaTimer, 1000);
+    }
+    private class PizzaTimer implements Runnable {
+        private Integer count = 0;
+        @Override
+        public void run() {
+            count++;
+            if (count > 4) {
+                orderStatus.postValue("Pizza ready to eat");
+            } else if (count > 3) {
+                orderStatus.postValue("Pizza is cooling");
+                handler.postDelayed(this, 2000);        // cool pizza for 2 seconds
+            } else if (count > 2) {
+                orderStatus.postValue("Pizza is baking");
+                handler.postDelayed(this, 5000);        // bake pizza for 5 seconds
+            } else {
+                orderStatus.postValue("Pizza is being prepared  ");
+                handler.postDelayed(this, 2000);        // wait 2 seconds for pizza to be prepared
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/cis/pizza/PizzaRepository.java
@@ -0,0 +1,91 @@
+package cis.pizza;
+
+
+import android.app.Application;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaRepository {
+    public static final Double DELIVERY_PRICE = 2.50;
+    private PizzaDao heartrateDao;
+    private List<Pizza> pizzasInOrder;          // list of pizzas ordered so far
+    private boolean delivery=false;             // true if customer wants order delivered
+
+    // Note that in order to unit test the WordRepository, you have to remove the Application
+    // dependency. This adds complexity and much more code, and this sample is not about testing.
+    // See the BasicSample in the android-architecture-components repository at
+    // https://github.com/googlesamples
+    PizzaRepository(Application application) {
+        PizzaDatabase db = PizzaDatabase.getDatabase(application);
+        heartrateDao = db.pizzaDao();
+        pizzasInOrder = new ArrayList<Pizza>();
+    }
+
+    /***
+     * Add an pizza to the order
+     * @return the description of the pizza is returned if needed
+     */
+    public String OrderPizza(Pizza newPizza){
+        // ROOM -- Add insert into database
+        PizzaDatabase.databaseWriteExecutor.execute(() -> {
+            heartrateDao.insert(newPizza);
+            //Log.d ("CIS 3334", newPizza.toString());
+        });
+        pizzasInOrder.add(newPizza);
+        return newPizza.toString();             // return a description of what was ordered
+    }
+
+    /***
+     * Retrieve the full order
+     * @return a list of pizzas, each one describe by a single string
+     */
+    public List<String> getOrder() {
+        PizzaDatabase.databaseWriteExecutor.execute(() -> {
+            pizzasInOrder = heartrateDao.getAll();              // ROOM - add this
+        });
+        ArrayList<String> strOrder = new ArrayList<String>();
+        for (Pizza p:pizzasInOrder ){
+            strOrder.add(p.toString());
+        }
+        return strOrder;
+    }
+
+    /***
+     * Retrieve one item from the order
+     * @return a list of pizzas, each one describe by a single string
+     */
+    public String getOrderItem(Integer position) {
+        Pizza currentPizza = pizzasInOrder.get(position);
+        return currentPizza.toString();
+    }
+
+    public Integer getOrderSize() {
+        return pizzasInOrder.size();
+    }
+
+    /***
+     * Calculate the total bill for this order
+     * @return total bill as Double
+     */
+    public Double getTotalBill() {
+        Double total = 0.0;
+        for (Pizza p:pizzasInOrder ){
+            total += p.getPrice();
+        }
+        if (delivery) {
+            total += DELIVERY_PRICE;
+        }
+        return total;
+    }
+
+    public void setDelivery(boolean deliver) {
+        this.delivery = deliver;
+    }
+
+    public boolean getDelivery() {
+        return delivery;
+    }
+}
